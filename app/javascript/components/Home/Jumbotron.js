f55ed377fb0919b3d33d415262c1b24c9f3ebfd6
karamalesa1980 <karamalesa@gmail.com>
2019-10-22 22:51:18 +0300

    add Jumbotron component (16:12)

---
+++ app/javascript/components/Home/Jumbotron.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Jumbotron = () => {
+  render (
+    <section className="home-section--1">
+      <div className="container">
+        <div className="row">
+          <div className="col col-sm-12 col-md-5"></div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Jumbotron
