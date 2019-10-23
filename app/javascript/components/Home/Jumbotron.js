f7a7396139edf0c0235d54856ba3d488c8bc4be7
karamalesa1980 <karamalesa@gmail.com>
2019-10-24 00:35:49 +0300

    add youtube player

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 
 const Jumbotron = () => {
-  render (
+  return (
     <section className="home-section--1">
       <div className="container">
         <div className="row">
-          <div className="col col-sm-12 col-md-5"></div>
+          <div className="col col-sm-12 col-md-5">
+            <div className="pt-4 mt-4">
+              <h1>React For Rails Developers</h1>
+              <p>Supercharge your Ruby on Rails Apps with React.js</p>
+            </div>
+          </div>
+          <div className="col col-sm-12 col-md-7">
+            <div className="pt-4 mt-4 text-center">
+            <iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="http://www.youtube.com/embed/aM8jH8pmmO0"
+  frameborder="0"/>
+            </div>
+          </div>
         </div>
       </div>
     </section>
   )
 }
 
-export default Jumbotron
+export default Jumbotron;
